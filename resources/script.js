6aacefe6f2526b9d2fe3bdd9a478f3348375e013
unknown <bgandreev@gmail.com>
2023-02-27 14:00:29 -0600

    Added functionality for formatting and saving to local storage

--- resources/script.js
+++ resources/script.js
@@ -12,6 +12,41 @@ $(document).ready(function () {
         localStorage.setItem(hour, userEntry);
     });
 
+    function rowFormatter() {
+
+        var currentHour = moment().hours();
+
+        // Loop through rows and add classes for formatting
+        $('.row').each(function () {
+
+        var rowTime = parseInt($(this).attr('id'));
+
+        if (rowTime < currentHour) {
+            $(this).addClass('past');
+        } else if (rowTime === currentHour) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future');
+        }
+        });
+    }
+
+    rowFormatter();
+
+    // Runs formatting function every 10 seconds
+    var interval = setInterval(rowFormatter, 10000);
+
+    // Display values from local storage
+    $('#9 .cal-entry').val(localStorage.getItem('9'));
+    $('#10 .cal-entry').val(localStorage.getItem('10'));
+    $('#11 .cal-entry').val(localStorage.getItem('11'));
+    $('#12 .cal-entry').val(localStorage.getItem('12'));
+    $('#13 .cal-entry').val(localStorage.getItem('13'));
+    $('#14 .cal-entry').val(localStorage.getItem('14'));
+    $('#15 .cal-entry').val(localStorage.getItem('15'));
+    $('#16 .cal-entry').val(localStorage.getItem('16'));
+    $('#17 .cal-entry').val(localStorage.getItem('17'));
+
 });
   
   
