6f008828a5758ddf2c811731f08e56c41ca2d93e
unknown <bgandreev@gmail.com>
2023-02-27 13:12:16 -0600

    Added functionality to save to local storage

--- resources/script.js
+++ resources/script.js
@@ -3,6 +3,15 @@ $(document).ready(function () {
     //Displays current day
     $('#currentDay').text(moment().format('dddd, MMMM Do,'));
 
+    // Capturing clicks of the save buttons and saving to local storage
+    $('.button').on('click', function () {
+
+        var hour = $(this).parent().attr('id');
+        var userEntry = $(this).siblings('.cal-entry').val();
+
+        localStorage.setItem(hour, userEntry);
+    });
+
 });
   
   
